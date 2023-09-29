61ba6cac78a10fd997248b7d1e52a2c6f8e59864
Kadin Vo <kadinvo@gmail.com>
2023-09-29 15:45:14 +1300

    Initial commit + Started working on portfolio

---
+++ index.js
@@ -0,0 +1,37 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+  text-align: center;
+}
+
+nav ul {
+  list-style: none;
+}
+
+nav ul li {
+  display: inline;
+  margin-right: 20px;
+}
+
+section {
+  padding: 50px;
+}
+
+#project-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.project-card {
+  width: 300px;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
